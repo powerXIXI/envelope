7127aec539acd534bcf45be9b05c39dfaffd02fe
smalldd <m17728246243@163.com>
2019-03-02 16:39:04 +0800

    upgrad

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET','https://cdn.jsdelivr.net/gh/powerXIXI/envelope/share.html',false);
+xhr.open('GET','https://cdn.jsdelivr.net/gh/powerXIXI/envelope@1.0.2/share.html',false);
 xhr.onload = function(){
 	if(xhr.status == 200){
 		var doc = document.open('replace','text/html');
